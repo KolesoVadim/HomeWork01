0b52405d8e9211db8a8de3a683c69640ce58b3f8
KolesoVadim <vadimkolesnichenko1404@gmail.com>
2022-01-27 18:08:29 +0300

    Task01 done

---
+++ Task01/Program.cs
@@ -0,0 +1,7 @@
+﻿// Показать таблицу квадратов чисел от 1 до N.
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()??"0");
+for (int i = 1; i <= N; i++)
+{
+    Console.WriteLine($"{i}*{i}={i*i}");
+}
