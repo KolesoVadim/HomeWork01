a037cfb4154931bfe7f0d817dfc4d8afca06cf1d
KolesoVadim <vadimkolesnichenko1404@gmail.com>
2022-01-27 18:40:21 +0300

    Task03 done

---
+++ Task03/Program.cs
@@ -0,0 +1,11 @@
+﻿// Возведите число А в натуральную степень B используя цикл.
+Console.Write("Введите число А: ");
+int A = int.Parse(Console.ReadLine()??"0");
+Console.Write("Введите степень В: ");
+int B = int.Parse(Console.ReadLine()??"0");
+int Result = 1;
+for (int i = 1; i <= B; i++)
+{
+    Result = Result * A;
+}
+Console.WriteLine ($"{A} в степени {B} = {Result}");
