4a69aad431286b72b94646bddb95923c9077a3cc
KolesoVadim <vadimkolesnichenko1404@gmail.com>
2022-01-28 18:37:35 +0300

    Task04 done

---
+++ Task04/Program.cs
@@ -0,0 +1,10 @@
+﻿// Определить количество цифр в числе.
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()??"0");
+int sum = 0;
+while (number > 0)
+{
+    number = number / 10;
+    sum++;
+}
+Console.WriteLine(sum);
