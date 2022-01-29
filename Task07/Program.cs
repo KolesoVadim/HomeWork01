6d4fbab17f8d12e53d67052298d8ba12d89798cd
KolesoVadim <vadimkolesnichenko1404@gmail.com>
2022-01-29 13:18:23 +0300

    Task07 done

---
+++ Task07/Program.cs
@@ -0,0 +1,9 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N.
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine()??"0");
+int prod = 1;
+for (int i = 2; i <= N; i++)
+{
+    prod = prod * i;
+}
+Console.WriteLine($"Произведение чисел равно " + prod);
